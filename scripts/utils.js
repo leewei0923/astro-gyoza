618e81a112fdedc4781c642f578996b477362d4c
leewei <2955538482@qq.com>
2024-05-30 15:52:03 +0800

    feat: 更新文章

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,4 @@
 export function isFileNameSafe(fileName) {
-  return /^[a-z0-9]+(-[a-z0-9]+)*$/.test(fileName)
+  // 正则表达式匹配中文字符、小写字母、数字和连字符
+  return /^[a-zA-Z0-9\u4e00-\u9fa5]+(-[a-zA-Z0-9\u4e00-\u9fa5]+)*$/.test(fileName)
 }
